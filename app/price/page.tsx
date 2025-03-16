34f2d324630ab09497e4fa5f2244bc9eab23e2a1
Ramazan <ramazandjaparov@gmail.com>
2025-03-16 10:15:49 +0600

    addMainPage

--- app/price/page.tsx
+++ app/price/page.tsx
@@ -1,3 +1,13 @@
+import { Accordion } from "@/components/accordion";
+import { Offer } from "@/components/offer";
+import { Additionally } from "@/components/priceAdditionally";
+
 export default function Price() {
-  return <div>Price</div>;
+  return (
+    <div>
+      <Offer />
+      <Additionally />
+      <Accordion />
+    </div>
+  );
 }
