80c058668293e708418f4c0c53f1bc1979ce6419
Ramazan <ramazandjaparov@gmail.com>
2025-03-17 09:16:19 +0600

    .

--- components/aboutFounderInfo/myVideo/index.tsx
+++ components/aboutFounderInfo/myVideo/index.tsx
@@ -1,7 +1,6 @@
 "use client";
 import styles from "./style.module.css";
 
-import ReactPlayer from "react-player";
 export const MyVideo = () => {
   return (
     <div>
@@ -12,14 +11,7 @@ export const MyVideo = () => {
         </h1>
         <div className={styles.borderBottom}></div>
       </div>
-      <div className={styles.video}>
-        <ReactPlayer
-          url="https://vimeo.com/1065926311/5cd22c1b65"
-          controls
-          width="100%"
-          height="100%"
-        />
-      </div>
+      <div className={styles.video}></div>
     </div>
   );
 };
--- components/aboutSlider/index.tsx
+++ components/aboutSlider/index.tsx
@@ -1,5 +1,10 @@
-import styles from "./style.module.css";
+"use client";
 
+import styles from "./style.module.css";
+import { sliderBase } from "./sliderBase";
+import { SwiperSlide, Swiper } from "swiper/react";
+import Image from "next/image";
+import { Autoplay, Pagination } from "swiper/modules";
 export const AboutSlider = () => {
   return (
     <>
@@ -11,7 +16,30 @@ export const AboutSlider = () => {
           </h1>
         </div>
       </div>
-      <div className={styles.sliderContainer}></div>
+      <div className={styles.sliderContainer}>
+        <Swiper
+          loop={true}
+          slidesPerView={1}
+          grabCursor={true}
+          centeredSlides={true}
+          pagination={{ clickable: true }}
+          autoplay={{ delay: 3000 }}
+          speed={1000}
+          modules={[Autoplay, Pagination]}
+          className={styles.customerSwiper}
+          breakpoints={{
+            1440: {
+              slidesPerView: 1,
+            },
+          }}
+        >
+          {sliderBase.map((item, id) => (
+            <SwiperSlide key={id}>
+              <Image src={item.image} width={1178} height={712} alt="people" />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </>
   );
 };
